93e24acbd7fe5180a1f2dd4e0d3e32c57ae46f83
Shaaams <shamsssherien@gmail.com>
2020-03-28 02:56:11 +0300

    first lecture

---
+++ app/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name','last_name', 'email', 'email_verified',
+        'mobile', 'mobile_verified', 'password', 'shipping_address',
+        'billing_address',
+
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    //ToDo:Relation Between User Table And Others Tables in DataBase
+
+    //ToDo:Relation Between User Table And Orders Table in DataBase
+    public function orders(){
+        return $this->hasMany(Order::class);
+    }
+
+    //ToDo:Relation Between User Table And Payments Table in DataBase
+    public function payments(){
+        return $this->hasMany(Payment::class);
+    }
+
+    //ToDo:Relation Between User Table And Shipments Table in DataBase
+    public function shipments(){
+        return $this->hasMany(Shipment::class);
+    }
+
+    //this user own One shippingAddress
+    public function shippingAddress()
+    {
+        return $this->hasOne(Address::class, 'shipping_address', 'id');
+    }
+
+    //this user own One billingAddress
+    public function billingAddress()
+    {
+        return $this->hasOne(Address::class, 'billing_address', 'id');
+    }
+
+
+
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Address::class, 1000)->create();
+        factory(\App\User::class, 500)->create();
+        factory(\App\Product::class, 1500)->create();
+        factory(\App\Image::class, 3500)->create();
+    }
+}
